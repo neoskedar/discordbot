08c3baf8853ce436bae6dcb9fa4496b7ca9fed26
EC2 Default User <ec2-user@ip-172-31-45-53.us-east-2.compute.internal>
2020-04-27 16:06:20 +0000

    1

--- commands/json.js
+++ commands/json.js
@@ -4,17 +4,19 @@ module.exports = {
 	name: 'json',
 	description: 'Reads a JSON file!',
 	execute(message) {
-        'use strict';
+        const fetch = require('node-fetch');
+      //  var fs = require('fs');
+        //var json = JSON.parse(fs.readFileSync('/home/ec2-user/Turnip-Bot/json/acnh-nintendo-switch.json', 'utf8'));
 
-        const fs = require('fs');
-        
-        fs.readFile('/home/ec2-user/Turnip-Bot/json/acnh-nintendo-switch.json', (err, data) => {
-            if (err) throw err;
-            let student = JSON.parse(data);
-            console.log(student);
+          //  console.log(json.games.nh.sellPrice.value);
 
-        });
-        
-        console.log('This is after the read call');
+            fetch('https://raw.githubusercontent.com/jefflomacy/villagerdb/master/data/items/acnh-nintendo-switch.json')
+             //raw content for the 1UP Cap, fetched from https://raw.githubusercontent.com/jefflomacy/villagerdb/master/data/items/1-up-cap.json
+            .then(response => response.json())
+            .then(data => {
+            console.log(data.games.nh.sellPrice.value);
+            })
+            .catch(error => console.error(error))
+
 	},
 };
